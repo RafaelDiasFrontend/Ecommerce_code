d3207312a457edf395b6a49f579b6fa5f173d410
Rafael Dias <contato.rafaeldiasfrontend.com.br>
2020-07-21 22:48:15 -0300

    1.2
    21-07

--- frontend/components/Cart/CartOverview.vue
+++ frontend/components/Cart/CartOverview.vue
@@ -1,3 +1,24 @@
 <template>
-  <div class="cart-overview">Cart</div>
+  <table class='grey lighten-4 w-100'>
+    <tbody>
+      <CartOverviewProduct 
+      v-for="product in products" 
+      :key="product.id" 
+      
+      />
+    </tbody>
+  </table>
 </template>
+<script>
+import CartOverviewProduct from "@/components/cart/CartOverviewProduct";
+
+import { mapGetters } from "vuex";
+export default {
+  components: { CartOverviewProduct },
+  computed: {
+    ...mapGetters({
+      products: "cart/products"
+    })
+  }
+};
+</script>
---
+++ frontend/components/Cart/CartOverviewProduct.vue
@@ -0,0 +1,16 @@
+<template>
+  <tr>
+    <td width="120">
+      <img src="http://via.placeholder.com/60x60" alt />
+    </td>
+    <td>Nome do Produto</td>
+    <td>Dropdow for quantity</td>
+    <td>Total</td>
+    <td>
+      <a href=""> X </a>
+    </td>
+  </tr>
+</template>
+<script>
+export default {};
+</script>
