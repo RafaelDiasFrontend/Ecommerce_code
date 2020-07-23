66e4bf18064bce2ad81f7cff4c6f167dc9425ec5
Rafael Dias <contato.rafaeldiasfrontend.com.br>
2020-07-23 15:02:34 -0300

    1.7
    limpando carrinho

--- frontend/pages/carrinho/index.vue
+++ frontend/pages/carrinho/index.vue
@@ -2,7 +2,7 @@
     <v-container class="fill-height" fluid>
       <v-row align="center" justify="center">
         <v-col cols="12" sm="8" md="4">
-          <v-card class="elevation-12">
+          <v-card class="elevation-12" v-if="products.length">
             <v-toolbar color="primary" dark flat>
               <v-toolbar-title class='h3'>Carrinho</v-toolbar-title>
               <v-spacer></v-spacer>
@@ -20,19 +20,31 @@
                 <CartOverview />
             </v-card-text>
             <v-card-actions>              
-              <v-btn color="primary" class='w-100 bg-primary-color white--text btn-register mt-1' large>Comprar</v-btn>
+              <v-btn :disabled="empty" color="primary" class='w-100 bg-primary-color white--text btn-register mt-1' large>Comprar</v-btn>
             </v-card-actions>
           </v-card>
+          <!-- If cart is empty -->
+          <div v-else class="text-center">
+            <h2 class='mb-3'>Ops! O seu carrinho esta vazio!</h2>
+            <nuxt-link class='grey--text' :to="{ name: 'loja' }">Voltar para a loja</nuxt-link>
+          </div>
         </v-col>
       </v-row>
     </v-container>
 </template>
 
 <script>
+import  { mapGetters } from 'vuex'
 import CartOverview from '@/components/Cart/cartOverview'
   export default {
       components: {
           CartOverview
-      } 
+      },   
+      computed: {
+        ...mapGetters({
+          empty: 'cart/empty',
+          products: 'cart/products'
+        })
+      }
   }
 </script>
