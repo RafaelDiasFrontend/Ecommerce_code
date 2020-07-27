91f68f3a01ffac92c8ceaa56ee62626a9d21013b
Rafael Dias <contato.rafaeldiasfrontend.com.br>
2020-07-27 20:19:32 -0300

    1.65
    guardando o endereço do user no banco

--- api/app/Http/Controllers/Addresses/AddressController.php
+++ api/app/Http/Controllers/Addresses/AddressController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Addresses;
 
-use App\Http\Controllers\Controller;
-use App\Http\Resources\AddressResource;
 use App\Models\Address;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AddressResource;
+use App\Http\Requests\Address\AddressStoreRequest;
 
 class AddressController extends Controller
 {
@@ -17,5 +18,18 @@ class AddressController extends Controller
     public function index(Request $request)
     {
        return AddressResource::collection($request->user()->addresses);
+    }
+
+    public function store(AddressStoreRequest $request)
+    {
+        $address = Address::make($request->only([
+            'name', 'address_1', 'city', 'postal_code', 'country_id'
+        ]));
+
+        $request->user()->addresses()->save($address);
+
+        return new AddressResource(
+           $address
+        );
     }
 }
