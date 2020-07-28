51e45f077ec4b6c0d6187e884939b567e64b3dfe
Rafael Dias <contato.rafaeldiasfrontend.com.br>
2020-07-28 01:21:11 -0300

    1.66.2
    checkout

---
+++ frontend/pages/checkout/index.vue
@@ -0,0 +1,62 @@
+<template>
+<section id='checkout' class='mg-section'>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="8">
+        <!-- Send To -->
+        <v-card class="my-1">
+          <h2 class="h2-custom">Enviar para</h2>
+        </v-card>
+        <!-- Payments -->
+        <v-card class="my-1">
+          <h2 class="h2-custom">Metodo De Pagamento</h2>
+        </v-card>
+        <!-- Shipping -->
+        <v-card class='my-1'>
+          <h2 class="h2-custom">Shipping</h2>
+          <v-form>
+            <v-select></v-select>
+          </v-form>
+        </v-card>
+        <v-spacer></v-spacer>
+        <v-card>
+          <h2 class='h2-custom'>Cart Summary</h2>
+          <v-form>
+            <v-select></v-select>
+          </v-form>
+        </v-card>
+
+        <v-btn
+          type="submit"
+          class="bg-secondary-color btn-register mt-5"
+          :disabled="!valid"
+          @click="validate"
+          required
+        >Realizar pagamento</v-btn>
+      </v-col>
+
+      <!-- Col 02 -->
+      <v-col cols="12" md="4">
+        <v-btn
+          type="submit"
+          class="bg-secondary-color btn-register mt-5"
+          :disabled="!valid"
+          @click="validate"
+          required
+        >Realizar pagamento</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</section>
+</template>
+
+<script>
+
+
+</script>
+
+
+
+<style scoped>
+
+</style>
