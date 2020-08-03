cfbec1a031bc0718d6e2a19a31b4aa75dc2cc0ce
Rafael Dias <contato.rafaeldiasfrontend.com.br>
2020-08-03 13:59:01 -0300

    1.71
    Lista de paises no backend

--- api/app/Http/Resources/CountryResource.php
+++ api/app/Http/Resources/CountryResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CountryResource extends JsonResource
--- api/routes/api.php
+++ api/routes/api.php
@@ -5,6 +5,8 @@
 Route::resource('categories', 'Categories\CategoryController');
 Route::resource('products', 'Products\ProductController');
 Route::resource('addresses', 'Addresses\AddressController');
+Route::resource('countries', 'Countries\CountryController');
+
 
 
 
