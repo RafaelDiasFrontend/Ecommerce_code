5f62c52648d0fe8d466a5499401b4da7755adf08
Rafael Dias <contato.rafaeldiasfrontend.com.br>
2020-08-04 15:23:39 -0300

    1.77

--- frontend/pages/checkout/index.vue
+++ frontend/pages/checkout/index.vue
@@ -3,14 +3,9 @@
     <v-container>
       <v-row>
         <v-col cols="12" md="8">
-          {{ form }}
           <!-- Send To -->
-          <v-card class="mb-5">          
-            <ShippingAddress 
-            :addresses="addresses"
-            v-model="form.address_id"
-
-            />
+          <v-card class="mb-5">
+            <ShippingAddress :addresses="addresses" v-model="form.address_id" />
           </v-card>
           <!-- Payments -->
           <v-card class="mb-5">
@@ -19,9 +14,17 @@
           <!-- Shipping -->
           <v-card class="mb-5">
             <h2 class="h2-custom">Shipping</h2>
-            <v-form>
-              <v-select></v-select>
-            </v-form>
+            <form>                      
+              <select v-model="form.shipping_id">
+                <option
+                  v-for="shipping in shippingMethods"
+                  :key="shipping.id"
+                  :value="shipping.id"
+                >
+                {{ shipping.name }} ({{ shipping.price }})
+                </option>
+              </select>
+            </form>
           </v-card>
           <v-spacer></v-spacer>
 
@@ -52,7 +55,7 @@
           <v-btn
             type="submit"
             class="bg-secondary-color btn-register mt-4"
-            :disabled="empty"          
+            :disabled="empty"
             required
           >Realizar pagamento</v-btn>
         </v-col>
@@ -80,16 +83,23 @@ import ShippingAddress from "@/components/Checkout/Addresses/ShippingAddress";
 export default {
   components: {
     CartOverview,
-    ShippingAddress
+    ShippingAddress,
   },
   data() {
     return {
       addresses: [],
+      shippingMethods: [],
       form: {
-        address_id: null
-      }
-    }
-  }, 
+        address_id: null,
+        shipping_method_id: null
+      },
+    };
+  },
+  watch: {
+    'form.address_id' (addressId) {
+      this.getShippinMethodsForAddress(addressId);
+    },
+  },
   computed: {
     ...mapGetters({
       total: "cart/total",
@@ -97,13 +107,22 @@ export default {
       empty: "cart/empty",
     }),
   },
+
+  methods: {
+    async getShippinMethodsForAddress(addressId) {
+      let response = await this.$axios.$get(`addresses/${addressId}/shipping`);
+
+      this.shippingMethods = response.data;
+    },
+  },
+
   async asyncData({ app }) {
-   let addresses = await app.$axios.$get('addresses')
+    let addresses = await app.$axios.$get("addresses");
 
     return {
-      addresses: addresses.data
-    }
-  }
+      addresses: addresses.data,
+    };
+  },
 };
 </script>
 
