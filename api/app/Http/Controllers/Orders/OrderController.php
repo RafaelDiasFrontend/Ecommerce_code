e8cb45966159a2cd17c8de13f5fd1a245023e3a9
Rafael Dias <contato.rafaeldiasfrontend.com.br>
2020-08-12 00:36:40 -0300

    1.83

--- api/app/Http/Controllers/Orders/OrderController.php
+++ api/app/Http/Controllers/Orders/OrderController.php
@@ -18,17 +18,8 @@ class OrderController extends Controller
     public function store(OrderStoreRequest $request)
     {
     return [
-        dd('a')
-        // 'address_id' => [
-        //     'required',
-        //     Rule::exists('addresses', 'id')->where(function ($builder) {
-        //         $builder->where('user_id', $this->user()->id);
-        //     })
-        // ],
-        // 'shipping_method_id' => [
-        //     'required',
-        //     'exists:shipping_methods,id'
-        //             ]
+        dd('a')     
+     
 
     ];
     }
--- api/app/Http/Requests/Orders/OrderStoreRequest.php
+++ api/app/Http/Requests/Orders/OrderStoreRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests\Orders;
 
+use App\Models\Address;
 use Illuminate\Validation\Rule;
+use App\Rules\ValidShippingMethod;
 use Illuminate\Foundation\Http\FormRequest;
 
 class OrderStoreRequest extends FormRequest
@@ -23,7 +25,8 @@ class OrderStoreRequest extends FormRequest
      * @return array
      */
     public function rules()
-    {
+    {    
+
         return [
             'address_id' => [
                 'required',
@@ -31,6 +34,11 @@ class OrderStoreRequest extends FormRequest
                     $builder->where('user_id', $this->user()->id);
                 })
             ],
+            'shipping_method_id' => [
+                'required',
+                'exists:shipping_methods,id',
+                new ValidShippingMethod($this->address_id)
+            ]
         ];
     }
 }
